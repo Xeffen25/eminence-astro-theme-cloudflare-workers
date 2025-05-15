e1f311e62c3f209966056fdd12d1e470d5aa54ef
xeffen25 <xeffen25@xeffen25.com>
2025-05-15 18:29:32 +0000

    Robots and Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://eminence-astro-workers.xeffen25.com/",
+
 	i18n: {
 		defaultLocale: "es",
 		locales: ["es"],
@@ -21,5 +24,7 @@ export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
 	output: "server",
+	integrations: [sitemap(), robotsTxt()],
 });
