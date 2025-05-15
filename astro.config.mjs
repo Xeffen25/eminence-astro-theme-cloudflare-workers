f9eabdb8bcd73a68682abbcfe3547fa6ef20b452
xeffen25 <xeffen25@xeffen25.com>
2025-05-15 17:05:30 +0000

    Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+	i18n: {
+		defaultLocale: "es",
+		locales: ["es"],
+	},
+
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
